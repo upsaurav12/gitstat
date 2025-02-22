7d2e6f725477bc68370d2d77898730fca034f35a
upsaurav12 <sauravup041103@gmail.com>
2025-02-22 16:48:23 +0530

    add: nextAuth and shadcn/ui
    Signed-off-by: upsaurav12 <sauravup041103@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,15 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
+<<<<<<< HEAD
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+=======
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+>>>>>>> e4504bb (Initial commit from Create Next App)
   ],
   theme: {
     extend: {
