5b39a2ca4b6072abfe173e72504e721e0c767adb
upsaurav12 <sauravup041103@gmail.com>
2025-02-22 16:53:30 +0530

    fix: removed println
    Signed-off-by: upsaurav12 <sauravup041103@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,18 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-<<<<<<< HEAD
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-=======
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
->>>>>>> e4504bb (Initial commit from Create Next App)
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
